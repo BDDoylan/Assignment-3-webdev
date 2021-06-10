609dea1986dd3b813e23cdf2922a93d9390d259d
BDDoylan <bddoylan@gmail.com>
2021-06-10 11:42:32 -0400

    Initial commit and implementation of adding rows to grid

---
+++ script.js
@@ -0,0 +1,48 @@
+let numRows = 0;
+let numCols = 0;
+let colorSelected; 
+
+//Adds a row
+function addR() {
+
+    // Accessing the table element using the DOM
+    let grid = document.getElementById("grid");
+
+    // Create a <tr> inside of the grid
+    let row = grid.insertRow(0);
+
+    // Insert a cell in the 0th position of the row in the grid
+    row.insertCell(0);
+
+    console.log("Clicked Add Row")
+}
+//Adds a column
+function addC() {
+    console.log("Clicked Add Col")
+}
+
+//Removes a row
+function removeR() {
+    console.log("Clicked Remove Row")
+}
+//Remove a column
+function removeC() {
+    console.log("Clicked Remove Col")
+}
+//sets global var for selected color
+function selected(){
+    colorSelected = document.getElementById("selectedID").value;
+    console.log(colorSelected);
+}
+
+function fill(){
+    console.log("Clicked Fill All")
+}
+
+function clearAll(){
+    console.log("Clicked Clear All")
+}
+
+function fillU(){
+    console.log("Clicked Fill All Uncolored")
+}
